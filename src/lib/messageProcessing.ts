92f43089a6abf94570f2d541fd5f5b39020d96c5
Tomachi [ICHIGO] <lego@aol.jp>
2021-11-23 04:40:18 +0900

    fix: 削除された場合に動作鯖分全部で削除通知が飛ぶ不具合の修正

--- src/lib/messageProcessing.ts
+++ src/lib/messageProcessing.ts
@@ -1,25 +1,11 @@
 import mysql from 'mysql2/promise'
-import {
-  Message,
-  NewsChannel,
-  Snowflake,
-  TextChannel,
-  ThreadChannel,
-} from 'discord.js'
+import {Message, NewsChannel, Snowflake, TextChannel, ThreadChannel,} from 'discord.js'
 import os from 'os'
-import { check } from './checkData'
-import {
-  formatDate,
-  getDBChannel,
-  getDBGuild,
-  getDBMessage,
-  getDBThread,
-  getDBUser,
-  getDisplayContent,
-} from './utils'
+import {check} from './checkData'
+import {formatDate, getDBChannel, getDBGuild, getDBMessage, getDBThread, getDBUser, getDisplayContent,} from './utils'
 import Downloader from 'nodejs-file-downloader'
 import config from 'config'
-import { getClient } from '../main'
+import {getClient} from '../main'
 
 // --------------- メッセージ処理 --------------- //
 
@@ -31,50 +17,50 @@ import { getClient } from '../main'
  * @param past データベースにメッセージがないために作成する場合は true (過去メッセージとして)
  */
 export async function newMessage(
-  conn: mysql.Connection,
-  message: Message,
-  past = false
+    conn: mysql.Connection,
+    message: Message,
+    past = false
 ): Promise<void> {
   if (
-    !(
-      message.channel instanceof TextChannel ||
-      message.channel instanceof NewsChannel ||
-      message.channel instanceof ThreadChannel
-    )
+      !(
+          message.channel instanceof TextChannel ||
+          message.channel instanceof NewsChannel ||
+          message.channel instanceof ThreadChannel
+      )
   ) {
     return
   }
   console.log(
-    `New message${past ? '(past)' : ''}: ${message.cleanContent.substring(
-      0,
-      10
-    )} by ${message.author.tag} (${message.id}) in ${message.channel.name} (${
-      message.channelId
-    })`
+      `New message${past ? '(past)' : ''}: ${message.cleanContent.substring(
+          0,
+          10
+      )} by ${message.author.tag} (${message.id}) in ${message.channel.name} (${
+          message.channelId
+      })`
   )
 
   await check(conn, message)
 
   try {
     await conn.execute(
-      'INSERT INTO `message-createds` (msgid, displaytext, rawtext, guild_id, channel_id, thread_id, author_id, type, attachments, machine, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-      [
-        message.id,
-        getDisplayContent(message),
-        message.content,
-        message.guild?.id,
-        message.channel.isThread()
-          ? message.channel.parent?.id
-          : message.channelId,
-        message.channel.isThread() ? message.channelId : null,
-        message.author.id,
-        message.type,
-        message.attachments.size > 0
-          ? message.attachments.map((a) => a.url).join(',')
-          : null,
-        os.hostname(),
-        message.createdAt,
-      ]
+        'INSERT INTO `message-createds` (msgid, displaytext, rawtext, guild_id, channel_id, thread_id, author_id, type, attachments, machine, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        [
+          message.id,
+          getDisplayContent(message),
+          message.content,
+          message.guild?.id,
+          message.channel.isThread()
+              ? message.channel.parent?.id
+              : message.channelId,
+          message.channel.isThread() ? message.channelId : null,
+          message.author.id,
+          message.type,
+          message.attachments.size > 0
+              ? message.attachments.map((a) => a.url).join(',')
+              : null,
+          os.hostname(),
+          message.createdAt,
+        ]
     )
   } catch (e) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -90,9 +76,9 @@ export async function newMessage(
     const downloader = new Downloader({
       url: attachment.url,
       directory: `${
-        config.has('attachments_dir')
-          ? config.get('attachments_dir')
-          : 'attachments'
+          config.has('attachments_dir')
+              ? config.get('attachments_dir')
+              : 'attachments'
       }/${message.guildId}/${message.channelId}/${message.id}`,
       onProgress: function (percentage, _chunk, remainingSize) {
         console.log(`${percentage}% - Remaining bytes: ${remainingSize}`)
@@ -113,22 +99,22 @@ export async function newMessage(
  * @param message メッセージ
  */
 export async function editedMessage(
-  conn: mysql.Connection,
-  message: Message
+    conn: mysql.Connection,
+    message: Message
 ): Promise<void> {
   if (
-    !(
-      message.channel instanceof TextChannel ||
-      message.channel instanceof NewsChannel ||
-      message.channel instanceof ThreadChannel
-    )
+      !(
+          message.channel instanceof TextChannel ||
+          message.channel instanceof NewsChannel ||
+          message.channel instanceof ThreadChannel
+      )
   ) {
     return
   }
   console.log(
-    `Edited message: ${message.cleanContent.substring(0, 10)} by ${
-      message.author.tag
-    } (${message.id}) in ${message.channel.name} (${message.channelId})`
+      `Edited message: ${message.cleanContent.substring(0, 10)} by ${
+          message.author.tag
+      } (${message.id}) in ${message.channel.name} (${message.channelId})`
   )
 
   await check(conn, message)
@@ -144,15 +130,15 @@ export async function editedMessage(
 
   try {
     await conn.execute(
-      'INSERT INTO `message-editeds` (msgid, displaytext, rawtext, attachments, machine, timestamp) VALUES (?, ?, ?, ?, ?, ?)',
-      [
-        message.id,
-        getDisplayContent(message),
-        message.content,
-        message.attachments.map((a) => a.url).join(','),
-        os.hostname(),
-        message.editedAt,
-      ]
+        'INSERT INTO `message-editeds` (msgid, displaytext, rawtext, attachments, machine, timestamp) VALUES (?, ?, ?, ?, ?, ?)',
+        [
+          message.id,
+          getDisplayContent(message),
+          message.content,
+          message.attachments.map((a) => a.url).join(','),
+          os.hostname(),
+          message.editedAt,
+        ]
     )
   } catch (e) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -175,21 +161,29 @@ export async function editedMessage(
  * @param messageId Message ID
  */
 export async function deletedMessage(
-  conn: mysql.Connection,
-  guildId: Snowflake,
-  channelId: Snowflake,
-  messageId: Snowflake
+    conn: mysql.Connection,
+    guildId: Snowflake,
+    channelId: Snowflake,
+    messageId: Snowflake
 ) {
   console.log(
-    `Deleted message: Guild#${guildId} Channel#${channelId} Message#${messageId}`
+      `Deleted message: Guild#${guildId} Channel#${channelId} Message#${messageId}`
   )
 
-  await conn
-    .execute(
-      'INSERT INTO `message-deleteds` (msgid, machine, created_at) VALUES (?, ?, CURRENT_TIMESTAMP)',
-      [messageId, os.hostname()]
-    )
-    .catch(() => null)
+  try {
+    await conn
+        .execute(
+            'INSERT INTO `message-deleteds` (msgid, machine, created_at) VALUES (?, ?, CURRENT_TIMESTAMP)',
+            [messageId, os.hostname()]
+        )
+  } catch (e) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if (e.code === 'ER_DUP_ENTRY') {
+      return
+    }
+    console.error(e)
+  }
   await conn.commit()
 
   const message = await getDBMessage(conn, messageId)
@@ -209,9 +203,9 @@ export async function deletedMessage(
     return
   }
   const thread =
-    message.thread_id !== null
-      ? await getDBThread(conn, message.thread_id)
-      : null
+      message.thread_id !== null
+          ? await getDBThread(conn, message.thread_id)
+          : null
 
   // jMS Gamers Clubの場合のみ #deleted-messages に投げる
   if (guildId !== '597378876556967936') {
@@ -219,26 +213,26 @@ export async function deletedMessage(
   }
   const deletedMessageChannelId = config.get<string>('deletedMessageChannel')
   getClient()
-    .channels.fetch(deletedMessageChannelId)
-    .then(async (c) => {
-      if (!(c instanceof TextChannel)) {
-        return
-      }
-      const attachments =
-        message.attachments !== null &&
-        message.attachments.split(',').length > 0
-          ? '\n(' + message.attachments.split(',').length + 'ファイル)'
-          : ''
-      const userTag = `${user.username}#${user.discriminator}`
-      const threadOrChannel =
-        message.thread_id !== null && thread !== null
-          ? `<#${message.thread_id}> (\`${thread.name}\` of \`${channel.name}\` in \`${guild.name}\`)`
-          : `<#${message.channel_id}> (\`${channel.name}\` in \`${guild.name}\`)`
-      const datetime = formatDate(message.timestamp, 'yyyy/MM/dd HH:mm:ss')
-      const content = `\`\`\`${message.rawtext.replaceAll(
-        '`',
-        '\\`'
-      )}\`\`\`${attachments}\n-- at ${datetime} by ${userTag} - ${threadOrChannel}`
-      await c.send(content)
-    })
+      .channels.fetch(deletedMessageChannelId)
+      .then(async (c) => {
+        if (!(c instanceof TextChannel)) {
+          return
+        }
+        const attachments =
+            message.attachments !== null &&
+            message.attachments.split(',').length > 0
+                ? '\n(' + message.attachments.split(',').length + 'ファイル)'
+                : ''
+        const userTag = `${user.username}#${user.discriminator}`
+        const threadOrChannel =
+            message.thread_id !== null && thread !== null
+                ? `<#${message.thread_id}> (\`${thread.name}\` of \`${channel.name}\` in \`${guild.name}\`)`
+                : `<#${message.channel_id}> (\`${channel.name}\` in \`${guild.name}\`)`
+        const datetime = formatDate(message.timestamp, 'yyyy/MM/dd HH:mm:ss')
+        const content = `\`\`\`${message.rawtext.replaceAll(
+            '`',
+            '\\`'
+        )}\`\`\`${attachments}\n-- at ${datetime} by ${userTag} - ${threadOrChannel}`
+        await c.send(content)
+      })
 }
